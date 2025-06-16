4ff4d20d9819e9e4b8b9630afdb6289cd3eb2f8a
Goku <gokulgokuz21@gmail.com>
2025-06-16 12:41:31 +0530

    Host Updated

--- NewEnv/miniProject/miniProject/settings.py
+++ NewEnv/miniProject/miniProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-fs)mr4+l23ojghabkxl!drx$lk^*!fjruje%q_9whvm8i7sgci
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', 'project-s-a1mu.onrender.com']
 
 # Application definition
 
